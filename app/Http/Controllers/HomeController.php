184a11fa15ef30a67a643304c57077429e9a1488
reviwahyuabdillah <reviabdillah29@gmail.com>
2023-12-31 00:37:55 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use App\Models\Customer;
+use App\Models\Order;
+
+class HomeController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    public function index()
+    {
+        $bookCount = Book::count();
+        $customerCount = Customer::count();
+        $orderCount = Order::count();
+
+        return view('dashboard', compact('bookCount', 'customerCount', 'orderCount'));
+    }
+}
